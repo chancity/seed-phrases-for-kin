10cc27be70cf5b173cd416f7fe1110b846ca03fe
chancity <hawbakerchancey@gmail.com>
2019-03-31 01:11:34 -0700

    Updated to say kin

--- seed_phrases_for_kin/__init__.py
+++ seed_phrases_for_kin/__init__.py
@@ -1,5 +1,5 @@
 __all__ = [
-    'seed_phrase_to_stellar_keys',
+    'seed_phrase_to_kin_keys.py',
     'key_derivaton',
     'electrum_mnemonic'
     'old_electrum_mnemonic',
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def load(module_name, file_path):
 here = path.abspath(path.dirname(__file__))
 
 # Load the version module to get the version number
-load('version', path.join(here, 'seed_phrases_for_stellar/version.py'))
+load('version', path.join(here, 'seed_phrases_for_kin/version.py'))
 import version
 
 # Get the long description from the README file
@@ -26,21 +26,21 @@ with open(path.join(here, 'README'), encoding='utf-8') as f:
     long_description = f.read()
 
 if sys.version_info[:3] < (3, 5, 0):
-    sys.exit('Error: seed-phrases-for-stellar requires Python'
+    sys.exit('Error: seed-phrases-for-kin requires Python'
              ' version >= 3.5.0...')
 
 setup(
-    name='seed-phrases-for-stellar',
+    name='seed-phrases-for-kin',
     version=version.__version__,
     description='Utility for generating Stellar accounts from BIP39/Electrum seed phrases',
     long_description=long_description,
-    url='https://github.com/reverbel/seed-phrases-for-stellar',
-    author='Francisco Reverbel',
-    author_email='reverbel@gmail.com',
+    url='https://github.com/chancity/seed-phrases-for-kin',
+    author='chancity',
+    author_email='chancey@kinny.io',
     license="http://opensource.org/licenses/MIT",
     keywords='Stellar deterministic keypair generation '
              'BIP39 Electrum mnemonic seed phrase',
-    packages=['seed_phrases_for_stellar'],    
+    packages=['seed_phrases_for_kin'],
     install_requires=[
         'mnemonic>=0.18',
         'pbkdf2>=1.3',
@@ -48,8 +48,8 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-                'seed-phrase-to-stellar-keys'
-                '=seed_phrases_for_stellar.seed_phrase_to_stellar_keys:main',
+            'seed-phrase-to-stellar-keys'
+            '=seed_phrases_for_kin.seed_phrase_to_stellar_keys:main',
         ]
     },
     classifiers=[
